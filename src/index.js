718182671fb3ca000b42b64d139acc756f2d185b
krish <krishmaniyar27@gmail.com>
2025-01-11 17:44:16 +0530

    src

--- src/index.js
+++ src/index.js
@@ -35,18 +35,23 @@ function newProject(name) {
     inbox.set(name,project);
 }
 
+function deleteProject(name) {
+    inbox.delete(name);
+    resetpro();
+}
+
 const inbox = new Map();
 
 newProject('home');
 newProject('work');
 newProject('fitness');
 
-inbox.get('home').add("Organize Your Closet ","Sort clothes by season, donate those you no longer wear.","2025-09-27","Low");
-inbox.get('work').add("Organize Your Digital Workspace","Clean up your desktop, organize folders, and delete unnecessary files.","2025-09-27","Low");
-inbox.get('work').add("Set Clear Goals for the Week","Write down specific, measurable, and achievable goals for work.","2025-09-27","Low");
-inbox.get('work').add("Take an Online Course","Find a course related to your industry or a new skill you want to learn.","2025-09-27","Low");
-inbox.get('fitness').add("Full-Body Workout","Incorporate exercises like squats, push-ups, lunges, and planks to work multiple muscle groups.","2025-09-27","Low");
-inbox.get('fitness').add("Go for a Run or Jog ","If weather permits, go outside for a run or jog, or use a treadmill.","2025-09-27","Low");
+inbox.get('home').add("Organize Your Closet ","Sort clothes by season, donate those you no longer wear.","2025-09-27",true);
+inbox.get('work').add("Organize Your Digital Workspace","Clean up your desktop, organize folders, and delete unnecessary files.","2025-09-27",false);
+inbox.get('work').add("Set Clear Goals for the Week","Write down specific, measurable, and achievable goals for work.","2025-09-27",false);
+inbox.get('work').add("Take an Online Course","Find a course related to your industry or a new skill you want to learn.","2025-09-27",false);
+inbox.get('fitness').add("Full-Body Workout","Incorporate exercises like squats, push-ups, lunges, and planks to work multiple muscle groups.","2025-09-27",true);
+inbox.get('fitness').add("Go for a Run or Jog ","If weather permits, go outside for a run or jog, or use a treadmill.","2025-09-27",false);
 
 function makeleft() {
     const container = document.getElementById("left");
@@ -56,7 +61,12 @@ function makeleft() {
         let newElement = document.createElement("div");
         container.appendChild(newElement).className = 'project';
         newElement.innerHTML += "<button id=" + key + "><img src='./365f3e7f182c9f913994.svg' alt='project'><h1>" + key + "</h1></button>";
+        newElement.innerHTML += "<button class='option' id='delete" + key + "'><img src='./bde675c38d54979f589e.svg' alt='project'></button>";
+        container.innerHTML += '<br>';
     }
+    resetpro();
+    document.getElementById('ProjectDetails').style.display = 'none';
+    document.getElementById('mainleft').style.display = 'block';
 }
 
 function makeright() {
@@ -70,44 +80,111 @@ function makeright() {
             container.innerHTML += '<br>';
         }
     }
+    document.getElementById('taskDetails').style.display = 'none';
 }
 
 makeleft();
 makeright();
 
-document.getElementById('proadd').addEventListener("click",() => {
-    const name = prompt("Enter new project name:");
-    newProject(name);
+document.getElementById('proadd').addEventListener("click",()=> {
+    if(document.getElementById('ProjectDetails').style.display == 'block') {
+        document.getElementById('ProjectDetails').style.display = 'none';
+    }
+    else {
+        document.getElementById('ProjectDetails').style.display = 'block';
+    }
+});
+
+document.getElementById('cancel').addEventListener("click",() =>{
+    if(document.getElementById('ProjectDetails').style.display == 'block') {
+        document.getElementById('ProjectDetails').style.display = 'none';
+    }
+    else {
+        document.getElementById('ProjectDetails').style.display = 'block';
+    }
+    document.getElementById('proName').value = '';
+});
+
+function visdash() {
+    document.getElementById('dashboard').addEventListener("click",() =>{
+        if(document.getElementById('mainleft').style.display == 'block') {
+            document.getElementById('mainleft').style.display = 'none';
+        }
+        else if(document.getElementById('mainleft').style.display == 'none'){
+            document.getElementById('mainleft').style.display = 'block';
+        }
+    });
+}
+
+visdash();
+
+document.getElementById('getProject').addEventListener("click",() => {
+    newProject(document.getElementById('proName').value);
     makeleft();
     print(inbox);
+    document.getElementById('proName').value = '';
+    clickpro();
 });
 
 document.getElementById('addtask').addEventListener("click",() => {
-    const name = prompt("Enter new project name:");
-    const title = prompt("Enter new task title:");
-    const description = prompt("Enter new task description:");
-    const dueDate = prompt("Enter new task due date:");
-    const priority = prompt("Enter new task priority:");
-    inbox.get(name).add(title,description,dueDate,priority);
+    document.getElementById('taskDetails').style.display = 'block';
+});
+
+document.getElementById('getTask').addEventListener("click",() =>{
+    print( document.getElementById('title').value);
+    var title = document.getElementById('title').value;
+    var description = document.getElementById('description').value;
+    var dueDate = document.getElementById('date').value;
+    inbox.get('home').add(title,description,dueDate,false);
     makeright();
     print(inbox);
 });
 
-for(const [key] of inbox) {
-    document.getElementById(key).addEventListener("click",() =>{
-        const container = document.getElementById('details');
-        container.innerHTML = '';
-        for(let i of inbox.get(key).project) {
-            let newElement = document.createElement("div");
-            container.appendChild(newElement).className = 'task';
-            newElement.innerHTML = '<h1>' + i['title'] + '</h1>' + '<h1>:</h1>' + '<p>' + i['description'] +'</p>';
-            container.innerHTML += '<br>';
-        }
-    });   
+function resetpro() {
+    for(const [key] of inbox) {
+        document.getElementById('delete'+key).addEventListener("click",()=> {
+            deleteProject(key)
+            makeright();
+            makeleft();
+        });
+    }
 }
+
+function clickpro() {
+    for(const [key] of inbox) {
+        document.getElementById(key).addEventListener("click",() =>{
+            const container = document.getElementById('details');
+            container.innerHTML = '';
+            for(let i of inbox.get(key).project) {
+                let newElement = document.createElement("div");
+                container.appendChild(newElement).className = 'task';
+                newElement.innerHTML = '<h1>' + i['title'] + '</h1>' + '<h1>:</h1>' + '<p>' + i['description'] +'</p>';
+                container.innerHTML += '<br>';
+            }
+        });   
+    }
+}
+resetpro();
+clickpro();
 
 document.getElementById('inbox').addEventListener("click",()=>{
     makeright();
+});
+
+document.getElementById('important').addEventListener("click",()=>{
+    const container = document.getElementById('details');
+    container.innerHTML = '';
+    for(const [key] of inbox) {
+        for(let i of inbox.get(key).project) {
+            if(i['priority']) {
+                let newElement = document.createElement("div");
+                container.appendChild(newElement).className = 'task';
+                newElement.innerHTML = '<h1>' + i['title'] + '</h1>' + '<h1>:</h1>' + '<p>' + i['description'] +'</p>';
+                container.innerHTML += '<br>';
+            }
+        }
+    }
+    document.getElementById('taskDetails').style.display = 'none';
 });
 
 print(inbox);
